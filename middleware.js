d8d5be3f1f5ed6f1be85018bb6d67ee3dd9ba0e2
Emir <emir.boyadzhiev@student.odisee.be>
2026-06-02 11:35:31 +0200

    fix icons and middleware

--- middleware.js
+++ middleware.js
@@ -6,6 +6,6 @@ export async function middleware(request) {
 
 export const config = {
     matcher: [
-        '/((?!_next/static|_next/image|favicon.ico).*)',
+        '/((?!_next/static|_next/image|favicon.ico|sw.js|manifest.json|icons|screenshots).*)',
     ],
 };
